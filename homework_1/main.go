3d478563e4dc4b6a0b923812783dd420886d4016
mikhail <rybalkov@transtelesoft.com>
2024-05-02 16:00:37 +0300

    some changes homework 1

--- homework_1/main.go
+++ homework_1/main.go
@@ -5,13 +5,117 @@ import (
 )
 
 func img1() {
-	for x := 0; x < 24; x++ {
+	for x := 0; x < 25; x++ {
 		fmt.Println("")
-		for y := 0; y < 24; y++ {
+		for y := 0; y < 25; y++ {
 			if x < y {
-				fmt.Print("#")
+				fmt.Print("# ")
 			} else {
-				fmt.Print(".")
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img2() {
+	for x := 0; x < 25; x++ {
+		fmt.Println("")
+		for y := 0; y < 25; y++ {
+			if x == y {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img3() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if y == 24-x {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img5() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if x == y-x || x == y-x-1 {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img8() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if x*y == 0 {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img9() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if x < y-10 || y < x-10 {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img4() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if x+y < 30 {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
+			}
+		}
+	}
+	fmt.Println("")
+}
+
+func img24() {
+	var element_count = 25
+	for x := 0; x < element_count; x++ {
+		fmt.Println("")
+		for y := 0; y < element_count; y++ {
+			if x == y || x == 24-y {
+				fmt.Print("# ")
+			} else {
+				fmt.Print(". ")
 			}
 		}
 	}
@@ -19,5 +123,5 @@ func img1() {
 }
 
 func main() {
-	img1()
+	img5()
 }
