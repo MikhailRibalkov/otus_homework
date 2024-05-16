fb482118fa4007768e874ceac1e775bd46de30fc
mikhail <rybalkov@transtelesoft.com>
2024-05-16 18:23:41 +0300

    сделао решение в общем виде

--- lucky_tickets/main.go
+++ lucky_tickets/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func LuckyTickets6() int {
 	var count = 0
 	for a1 := 0; a1 < 10; a1++ {
@@ -20,6 +22,32 @@ func LuckyTickets6() int {
 	return count
 }
 
-func main() {
+type LuckyTickets struct {
+	count int64
+}
 
+func (lt *LuckyTickets) Next(n int, sumA int, sumB int) {
+	if n == 0 {
+		if sumA == sumB {
+			lt.count++
+		}
+		return
+	}
+
+	for a := 0; a < 10; a++ {
+		for b := 0; b < 10; b++ {
+			lt.Next(n-1, sumA+a, sumB+b)
+		}
+	}
+}
+
+func (Lt *LuckyTickets) LuckyTicketsCount(N int) int64 {
+	Lt.count = 0
+	Lt.Next(N, 0, 0)
+	return Lt.count
+}
+
+func main() {
+	lucky := LuckyTickets{}
+	fmt.Println(lucky.LuckyTicketsCount(6))
 }
--- lucky_tickets/main_test.go
+++ lucky_tickets/main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
 )
 
@@ -11,6 +15,52 @@ func TestLuckyTickets6(t *testing.T) {
 
 	if expected != result {
 		t.Errorf("Ожидалось число 55252, получено %d", result)
+	}
+
+}
+
+func TestLuckyTicketsCount(t *testing.T) {
+	var in int
+	var out int
+	lt := LuckyTickets{}
+
+	for i := 0; i < 5; i++ {
+		t.Logf("test %d\n", i)
+		fIn, err := os.Open(fmt.Sprintf("./1.Tickets/test.%d.in", i))
+		if err != nil {
+			t.Errorf("cannot open test file test.%d.in", i)
+		}
+		defer fIn.Close()
+
+		fOut, err := os.Open(fmt.Sprintf("./1.Tickets/test.%d.out", i))
+		if err != nil {
+			t.Errorf("cannot open test file test.%d.out", i)
+		}
+		defer fOut.Close()
+
+		sc := bufio.NewScanner(fIn)
+		sc.Scan()
+
+		in, err = strconv.Atoi(sc.Text())
+		if err != nil {
+			t.Error(err)
+		}
+
+		sco := bufio.NewScanner(fOut)
+		sco.Scan()
+
+		out, err = strconv.Atoi(sco.Text())
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		luckyCount := lt.LuckyTicketsCount(in)
+		if out != int(luckyCount) {
+			t.Errorf("Ошибка: ожидалость число %d, получено %d", out, luckyCount)
+		} else {
+			t.Logf("Из всех %d-значных билетов, %d счастливых\n", in*2, luckyCount)
+		}
 	}
 
 }
