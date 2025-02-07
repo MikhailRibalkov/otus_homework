d2118077b9c2eb43b135c7c40d68153e6c763e02
mikhail <rybalkov@transtelesoft.com>
2025-02-07 11:28:04 +0300

    selection and heap sort complete

--- selection_and_heap_sort/target/debug/deps/selection_and_heap_sort-c4e99454e2c9e838.d
+++
@@ -1,9 +0,0 @@
-/home/mikhail/RustProjects/otus_homework/selection_and_heap_sort/target/debug/deps/libselection_and_heap_sort-c4e99454e2c9e838.rmeta: src/main.rs Cargo.toml
-
-/home/mikhail/RustProjects/otus_homework/selection_and_heap_sort/target/debug/deps/selection_and_heap_sort-c4e99454e2c9e838.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
